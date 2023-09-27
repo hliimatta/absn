fdced857f655e946ae59240991eab8555ffaf59b
Harri Liimatta <harri@pulse247.info>
2023-09-27 06:57:29 +0300

    Refactor status_spec

--- spec/status_spec.cr
+++ spec/status_spec.cr
@@ -3,26 +3,23 @@ require "../src/status.cr"
 
 describe Status do
   it "returns correct status when working" do
-    Status.new(
-      TimespanRepository::Fake.new(
-        File.read("./spec/timespans_when_working.json")
-      )
-    ).print(CliOutput.new).content.should eq "Working since 14.06.2023 18:00"
+    statusmsg("./spec/timespans_when_working.json").should eq "Working since 14.06.2023 18:00"
   end
 
   it "returns correct status when not working" do
-    Status.new(
-      TimespanRepository::Fake.new(
-        File.read("./spec/timespans.json")
-      )
-    ).print(CliOutput.new).content.should eq "Last: 14.06.2023 - 1h 20m"
+    statusmsg("./spec/timespans.json").should eq "Last: 14.06.2023 - 1h 20m"
   end
 
   it "returns correct status when on a break" do
-    Status.new(
-      TimespanRepository::Fake.new(
-        File.read("./spec/timespans_when_on_break.json")
-      )
-    ).print(CliOutput.new).content.should eq "On a break since 14.06.2023 18:00"
+    statusmsg("./spec/timespans_when_on_break.json").should eq "On a break since 14.06.2023 18:00"
   end
 end
+
+def statusmsg(filename : String) : String
+    Status.new(
+      TimespanRepository::Fake.new(
+        File.read(filename)
+      )
+    ).print(CliOutput.new).content
+end
+
